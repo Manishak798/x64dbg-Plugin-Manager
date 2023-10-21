d4ef4dfa85b4746481416fc2cf78a9ad93ff555e
Manisha Kundnani <90680330+Manishak798@users.noreply.github.com>
2023-10-21 10:51:12 +0530

    Create diction.py

---
+++ diction.py
@@ -0,0 +1,15 @@
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+print(thisdict)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2000,
+  "brand":"BMW"
+  
+}
+print(thisdict)
